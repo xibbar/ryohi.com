f8b25ad4112b566e167a7253a1479c4e36533098
xibbar <xibbar@gmail.com>
2015-12-02 08:38:53 +0900

    デフォルトポート番号を3030に変更

--- config/boot.rb
+++ config/boot.rb
@@ -2,3 +2,14 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+require 'rails/commands/server'
+module Rails
+  class Server
+    def default_options
+      super.merge({
+        :Port => 3030
+      })
+    end
+  end
+end
