065b4fac330a79844ab6856cd5afe053ad7e0218
Takeyuki FUJIOKA <fujioka@rabbix.net>
2017-01-30 14:25:57 +0900

    refs #25 印刷ボタンをつけた

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,8 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Ryohiseisan::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
+if Rails.env == "development" || Rails.env == "test"
+  Ryohiseisan::Application.config.secret_key_base = "d98e5b878d3a762ce032ddc71006f3224131045330174f6e90a420080481abc1c30b1f73c3a89ccdcb79b073281ae0a9319cff812186a72bdfb5d859ed0d6a28"
+else
+  Ryohiseisan::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
+end
