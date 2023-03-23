c4a902286481f40153f93811b75f1d08a3f9b7e9
Mayuko Toda <toda@rabbix.jp>
2023-03-23 11:47:25 +0900

    turbolinks停止

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,7 +15,7 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 require("@rails/ujs").start()
-require("turbolinks").start()
+//require("turbolinks").start()
 require('jquery')
 
 require("../schedule")
